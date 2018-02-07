9be11cb31ab6a2164229ac34141b68265b6780be
Ricardo Lázaro <caffeine305@gmail.com>
2018-02-07 04:22:21 -0600

    +Enemigo Destruible
    Se agregó un icosaedro simplón que simula ser un enemigo. Ya está programado su comportamiento de tal manera que explota cuando un disparo lo toca y además se lleva al disparo...

---
+++ Assets/Scripts/OnContactDestroy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnContactDestroy : MonoBehaviour {
+
+    void OnTriggerEnter(Collider otherCol)
+    {
+        if (otherCol.tag == "Bounds")
+        {
+            return;
+        }
+
+        Destroy(otherCol.gameObject);
+        Destroy(this.gameObject);
+    }
+
+}
